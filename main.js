7efe076b1131e96fdb0869700bb4330d91fd9494
ARE-coder <82109141+ARE-coder@users.noreply.github.com>
2021-08-16 00:55:24 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function submit(){
+    document.getElementById("BDY").style.display = "block";
+    var a = document.getElementById("B").value; 
+}
+function BD(){
+    var a = document.getElementById("B").value; 
+    alert("HAPPY BIRTHDAY TO " + a ) 
+    document.getElementById("Birth").style.display = "block";
+    
+}
